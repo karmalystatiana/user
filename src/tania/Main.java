62c0badb2d023efc1691ee8f781295c1e65102ad
Tanya <karmalyst@gmail.com>
2022-02-23 09:41:17 +0300

    use

---
+++ src/tania/Main.java
@@ -0,0 +1,12 @@
+package tania;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя");
+        String name = scanner.next();
+        System.out.println("Добро пожаловать " + name);
+        }
+    }
